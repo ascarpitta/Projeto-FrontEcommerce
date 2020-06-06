de4e5cacc7a97a136c1938722f31a797971fb3e9
AMANDA SCARPITTA <41710169@mackenzista.com.br>
2020-06-06 15:42:43 -0300

    Aspectos de qualidade: correção do erro "Strings must use doublequote." em alterar-endereco.js

--- Scripts/alterar-endereco.js
+++ Scripts/alterar-endereco.js
@@ -1,41 +1,41 @@
-﻿var altendereco = document.getElementById('altendereco');
+﻿var altendereco = document.getElementById("altendereco");
 var endereco;
 var user;
 
-altendereco.addEventListener('submit', function (e) {
+altendereco.addEventListener("submit", function (e) {
 
     var obj = new FormData(altendereco);
     console.debug(obj);
-    var end = JSON.stringify(obj.get('end'));
-    var nomeEnd = JSON.stringify(obj.get('nomeEnd'));
-    var uf = JSON.stringify(obj.get('uf'));
-    var cidade = JSON.stringify(obj.get('cidade'));
-    var cep = JSON.stringify(obj.get('cep'));
-    var bairro = JSON.stringify(obj.get('bairro'));
-    var rua = JSON.stringify(obj.get('rua'));
-    var numero = JSON.stringify(obj.get('numero'));
-    var complemento = JSON.stringify(obj.get('complemento'));
-    var complemento = JSON.stringify(obj.get('obs'));
+    var end = JSON.stringify(obj.get("end"));
+    var nomeEnd = JSON.stringify(obj.get("nomeEnd"));
+    var uf = JSON.stringify(obj.get("uf"));
+    var cidade = JSON.stringify(obj.get("cidade"));
+    var cep = JSON.stringify(obj.get("cep"));
+    var bairro = JSON.stringify(obj.get("bairro"));
+    var rua = JSON.stringify(obj.get("rua"));
+    var numero = JSON.stringify(obj.get("numero"));
+    var complemento = JSON.stringify(obj.get("complemento"));
+    var complemento = JSON.stringify(obj.get("obs"));
 
     console.debug({ end, nomeEnd, uf, cidade, cep, bairro, rua, numero, complemento, obs });
 
     e.preventDefault();
-    console.debug('Ocorreu um click');
+    console.debug("Ocorreu um click");
 });
 
 function usuario() {
-    endereco = window.localStorage.getItem('endereco');
+    endereco = window.localStorage.getItem("endereco");
     console.log("endereço fora da função: ", endereco);
-    user = window.localStorage.getItem('user');
+    user = window.localStorage.getItem("user");
     console.log("id fora da função: ", user);
 }
 
 function endereco() {
     usuario();
     $.ajax({
-        url: 'https://projeto-ecommerce.herokuapp.com/api/enderecos/' + user,
-        type: 'get',
-        dataType: 'json',
+        url: "https://projeto-ecommerce.herokuapp.com/api/enderecos/" + user,
+        type: "get",
+        dataType: "json",
         success: function (url) {
             console.log(url);
             help = url[0].id;
@@ -63,12 +63,12 @@ function endereco() {
 function editar() {
     usuario();
     $.ajax({
-        url: 'https://projeto-ecommerce.herokuapp.com/api/enderecos/alterarendereco/' + user + '/' + $('#end').val().replace(/\s/g, '') + '/' + $('#nomeEnd').val().replace(/\s/g, '') + '/' + $('#cep').val().replace(/\s/g, '') + '/' + $('#uf').val().replace(/\s/g, '') + '/' + $('#cidade').val().replace(/\s/g, '') + '/' + $('#bairro').val().replace(/\s/g, '') + '/' + $('#rua').val().replace(/\s/g, '') + '/' + $('#numero').val().replace(/\s/g, '') + '/' + $('#complemento').val().replace(/\s/g, '') + '/' + $('#obs').val().replace(/\s/g, ''),
-        type: 'get',
-        dataType: 'json',
+        url: "https://projeto-ecommerce.herokuapp.com/api/enderecos/alterarendereco/" + user + "/" + $("#end").val().replace(/\s/g, "") + "/" + $("#nomeEnd").val().replace(/\s/g, "") + "/" + $("#cep").val().replace(/\s/g, "") + "/" + $("#uf").val().replace(/\s/g, "") + "/" + $("#cidade").val().replace(/\s/g, "") + "/" + $("#bairro").val().replace(/\s/g, "") + "/" + $("#rua").val().replace(/\s/g, "") + "/" + $("#numero").val().replace(/\s/g, "") + "/" + $("#complemento").val().replace(/\s/g, "") + "/" + $("#obs").val().replace(/\s/g, ""),
+        type: "get",
+        dataType: "json",
         success: function (url) {
             console.log(url);
-            alert('Endereço atualizado com sucesso');
+            alert("Endereço atualizado com sucesso");
             id = url.id;
             user = id;
             usuario();
@@ -79,20 +79,20 @@ function editar() {
 
 function verificar() {
     $.ajax({
-        url: 'https://projeto-ecommerce.herokuapp.com/api/enderecos/viacep/' + $('#cep').val(),
-        type: 'get',
-        dataType: 'json',
+        url: "https://projeto-ecommerce.herokuapp.com/api/enderecos/viacep/" + $("#cep").val(),
+        type: "get",
+        dataType: "json",
         success: function (url) {
             console.log(url);
             id = url.id;
             user = id;
             usuario();
             window.localStorage.setItem("user", user);
-            document.querySelector('input[name=complemento]').value = url.complemento;
-            document.querySelector('input[name=bairro]').value = url.bairro;
-            document.querySelector('input[name=cidade]').value = url.cidade;
-            document.querySelector('input[name=rua]').value = url.rua;
-            document.querySelector('input[name=uf]').value = url.uf;
+            document.querySelector("input[name=complemento]").value = url.complemento;
+            document.querySelector("input[name=bairro]").value = url.bairro;
+            document.querySelector("input[name=cidade]").value = url.cidade;
+            document.querySelector("input[name=rua]").value = url.rua;
+            document.querySelector("input[name=uf]").value = url.uf;
         }
     });
 }
