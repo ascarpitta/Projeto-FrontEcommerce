e0d25b04497679190d8c02f9f154a6554e0a2263
Juan Juan <juannvictor1998@gmail.com>
2020-06-21 18:42:31 -0300

    Atualizações de cadastro de produtos e página principal

---
+++ cad-produto.js
@@ -0,0 +1,58 @@
+﻿var cadproduto = document.getElementById("cadproduto");
+var user = window.localStorage.getItem("user");
+
+cadproduto.addEventListener("submit", function (e) {
+    var obj = new FormData(cadproduto);
+
+    var nome = JSON.stringify(obj.get("nome"));
+    var marca = JSON.stringify(obj.get("marca"));
+    var categoria = JSON.stringify(obj.get("categoria"));
+    var descricao = JSON.stringify(obj.get("descricao"));
+    var preco = JSON.stringify(obj.get("preco"));
+    var quantidade = JSON.stringify(obj.get("quantidade"));
+    var frete = JSON.stringify(obj.get("frete"));
+
+    console.debug({ nome, marca, categoria, descricao, preco, quantidade, frete });
+
+    e.preventDefault();
+});
+
+function cadProduto() {
+    $.ajax({
+        url: "https://projeto-ecommerce.herokuapp.com/api/produtos/CadastroProduto/" + user + "/" +
+            $("#nome").val().replace(/\s/g, "") + "/" + $("#descricao").val().replace(/\s/g, "") +
+            "/" + $("#preco").val().replace(/\s/g, "") + "/" + $("#frete").val().replace(/\s/g, "") +
+            "/" + $("#quantidade").val().replace(/\s/g, "") + "/" + $("#categoria").val().replace(/\s/g, "") +
+            "/" + $("#marca").val().replace(/\s/g, ""),
+        type: "GET",
+        dataType: "json",
+        success(url) {
+            alert("roduto cadastrado com sucesso"); 
+
+            window.localStorage.setItem("user", user);
+        }
+    });
+}
+
+function imagem() {
+    var form = new FormData();
+    form.append("imagem", fileInput.files[0], "https://projeto-ecommerce.herokuapp.com/api/Produtos/Imagem/Armazenar/")
+
+    var settings = {
+        url: "https://projeto-ecommerce.herokuapp.com/api/Produtos/Imagem/Armazenar/" + cadproduto + "/",
+        "method": "GET",
+        "timeout": 0,
+        "processData": false,
+        "mimeType": "multipart/form-data",
+        "contentType": false,
+        "data": form,
+    }
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
+
+function sair() {
+    sessionStorage.clear();
+    localStorage.clear();
+}
